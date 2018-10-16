130c4ab279813fcdf5eb97a5687d4209b15a22c9
liuleiy <1655425171@qq.com>
2018-10-16 20:07:15 +0800

    tijao

--- src/main/java/com/happyProject/admin/HappyProjectApplication.java
+++ src/main/java/com/happyProject/admin/HappyProjectApplication.java
@@ -14,6 +14,8 @@ public class HappyProjectApplication extends SpringBootServletInitializer
 	public static void main(String[] args) {
 		SpringApplication.run(HappyProjectApplication.class, args);
 	}
+	
+	
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         // 注意这里要指向原先用main方法执行的Application启动类
