f55705ce304deb35af79672bc1086efdb1156387
liuleiy <1655425171@qq.com>
2018-10-30 10:17:53 +0800

    提交

--- src/test/java/com/happyProject/admin/HappyProjectApplicationTests.java
+++ src/test/java/com/happyProject/admin/HappyProjectApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.happyProject.admin.utlis.Md5Util;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HappyProjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		String md5String = Md5Util.getMd5String("qwm12.as");
+		System.out.println(md5String);
 	}
 
 }
